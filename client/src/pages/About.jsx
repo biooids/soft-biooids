cdcd04c7da4d3110b242ca87e859f9cc528d8f0a
biooids <biooidsbiooid@gmail.com>
2024-06-08 03:41:21 +0200

    Update packages and create contact form

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import { Button, Checkbox, Label, TextInput, Textarea } from "flowbite-react";
+import { Link } from "react-router-dom";
 
 function About() {
   return (
-    <div className="min-h-screen flex items-center justify-center">
-      <div className="max-w-2xl mx-auto p-3 ">
+    <div className="min-h-screen ">
+      <div className="max-w-2xl m-auto p-3 ">
         <div>
           <h1 className="text-3xl font-semibold text-center my-7">
             About my blog
@@ -31,6 +33,74 @@ function About() {
               footprint together.
             </p>
           </div>
+        </div>
+        <div className="dark:text-cyan-500">
+          <form className="flex max-w-md flex-col gap-4">
+            <div>
+              <div className="mb-2 block">
+                <Label
+                  htmlFor="email2"
+                  value="Your email"
+                  className="dark:text-cyan-500"
+                />
+              </div>
+              <TextInput
+                id="email2"
+                type="email"
+                placeholder="name@flowbite.com"
+                required
+                shadow
+              />
+            </div>
+            <div>
+              <div className="mb-2 block">
+                <Label
+                  htmlFor="firstName"
+                  value="Your First Name"
+                  className="dark:text-cyan-500"
+                />
+              </div>
+              <TextInput id="firstName" type="text" required shadow />
+            </div>
+            <div>
+              <div className="mb-2 block">
+                <Label
+                  htmlFor="lastName"
+                  value="Your Last Name"
+                  className="dark:text-cyan-500"
+                />
+              </div>
+              <TextInput id="lastName" type="text" required shadow />
+            </div>
+            <div className="flex items-center gap-2">
+              <Checkbox id="agree" required />
+              <Label htmlFor="agree" className="flex dark:text-cyan-500">
+                I agree with the&nbsp;
+                <Link
+                  to="/terms"
+                  className="text-cyan-600 hover:underline dark:text-cyan-500"
+                >
+                  terms and conditions
+                </Link>
+              </Label>
+            </div>
+            <div className="max-w-md">
+              <div className="mb-2 block">
+                <Label
+                  htmlFor="comment"
+                  value="Your message"
+                  className="dark:text-cyan-500"
+                />
+              </div>
+              <Textarea
+                id="comment"
+                placeholder="Leave a comment..."
+                required
+                rows={4}
+              />
+            </div>
+            <Button type="submit">Contact me</Button>
+          </form>
         </div>
       </div>
     </div>
